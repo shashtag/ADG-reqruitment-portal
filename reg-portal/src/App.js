46ecfec8456d7f68ad3cafed79fbe723357b35cd
Aviroop Nandy <60533497+AviroopNandy@users.noreply.github.com>
2020-12-11 11:38:35 +0530

    Update App.js

--- reg-portal/src/App.js
+++ reg-portal/src/App.js
@@ -8,20 +8,22 @@ import Instructions from './components/Forms/Domain&Instructions/Instructions';
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Quiz from "./components/Quiz/Quiz";
 import Admin from './containers/Admin/Admin';
+import AdminLogin from './containers/Login/Login';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" exact component={Landing} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/signup2" component={SignUp2} />
-        <Route path="/login" component={Login} />
-        <Route path="/selection" component={DomainPage} />
-        <Route path="/instructions" component={Instructions} />
-        <Route path = "/quiz" component = {Quiz} />
-        <Route path = "/admin" component = {Admin} />
+        <Route path="/" exact component={ Landing } />
+        <Route path="/signup" component={ SignUp } />
+        <Route path="/signup2" component={ SignUp2 } />
+        <Route path="/login" component={ Login } />
+        <Route path="/selection" component={ DomainPage } />
+        <Route path="/instructions" component={ Instructions } />
+        <Route path="/quiz" component={ Quiz } />
+        <Route path="/admin" component={ Admin } />
+        <Route path="/admin-login" component={ AdminLogin } />
       </Router>
     </div>
   );
