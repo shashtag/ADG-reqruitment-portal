b4131a2f52873a59b8c6d063ca518b5a082f1acb
Shashwat Gupta <shashwatsatna@gmail.com>
2020-12-14 15:40:15 +0530

    bug fix instruction page

--- reg-portal/src/components/Forms/Domain&Instructions/Instructions.jsx
+++ reg-portal/src/components/Forms/Domain&Instructions/Instructions.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Background from "../../../hoc/Background/Background";
-import classes from './styles.module.css';
+import classes from "./styles.module.css";
 
-const Instructions = (props)=>{
-    return(
-        <Background>
-            <div className={classes.container}>
-                <h1>{props.location.param} Quiz</h1>
-                <p>Please read the instructions carefully before attempting the quiz</p>
-                <h1>Instructions</h1>
-                <ul>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos recusandae doloribus deleniti quasi repellat ipsum, amet quidem in</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex quos illum reprehenderit quo dignissimos dolores error? Deleniti quo voluptas</li>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, itaque unde aliquid nulla </li>
-                </ul>
-                <Link to='/quiz' className={classes.button}>Begin</Link>
-            </div>
-        </Background>
-    )
-}
+const Instructions = (props) => {
+  const linkTo = {
+    pathname: "/quiz",
+    param: props.location.param,
+  };
+  return (
+    <Background>
+      <div className={classes.container}>
+        <h1>{props.location.param} Quiz</h1>
+        <p>Please read the instructions carefully before attempting the quiz</p>
+        <h1>Instructions</h1>
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            recusandae doloribus deleniti quasi repellat ipsum, amet quidem in
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ex
+            quos illum reprehenderit quo dignissimos dolores error? Deleniti quo
+            voluptas
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, itaque
+            unde aliquid nulla{" "}
+          </li>
+        </ul>
+        <Link to={linkTo} className={classes.button}>
+          Begin
+        </Link>
+      </div>
+    </Background>
+  );
+};
 export default Instructions;
