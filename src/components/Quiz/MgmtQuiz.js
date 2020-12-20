ec9db86758da20d0d802d457cbd786f60833bd44
Aviroop Nandy <60533497+AviroopNandy@users.noreply.github.com>
2020-12-20 22:31:15 +0530

    update quiz files

--- src/components/Quiz/MgmtQuiz.js
+++ src/components/Quiz/MgmtQuiz.js
@@ -5,31 +5,35 @@ import Background from "../../hoc/Background/Background";
 import { Redirect } from "react-router-dom";
 import Modal from "../Modals/Modal";
 
-
 class MgmtQuiz extends React.Component {
+    selectedOptions = [];
     constructor(props) {
         super(props);
         this.state =  {
             quizQuestions: [],
             time: 600,
             currentQuestionIndex: 0,
-            selectedOptions: [],
-            questionId: []
+            questionId: [],
+            showModal:false
         };
+        this.submitQuiz = this.submitQuiz.bind(this);
         this.setSelectedOption = this.setSelectedOption.bind(this);
-        this.showModal1=this.showModal1.bind(this);
-        this.hideModal=this.hideModal.bind(this);
+        this.showModal1 = this.showModal1.bind(this);
+        this.hideModal = this.hideModal.bind(this);
     }
+
     showModal1(){
         this.setState({
             showModal:true
         })
     }
+
     hideModal(){
         this.setState({
             showModal:false
         })
     }
+
     getTimer() {
         if (this.state.time > 0) {
             setTimeout(() => {
@@ -63,24 +67,55 @@ class MgmtQuiz extends React.Component {
         })
     }
 
+    async submitQuiz() {
+        // const quizResponse = {qid: this.state.questionId, response: this.state.selectedOptions }
+        console.log("Inside submitQuiz", this.selectedOptions);
+        await fetch("https://adgrecruitments.herokuapp.com/user/management/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": sessionStorage.getItem("Token"),
+            },
+            body: JSON.stringify(this.selectedOptions),
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.log(error.message);
+        })
+    }
+
     optionsArray = ["a", "b", "c", "d"];
 
-    setSelectedOption() {
-        console.log("Hi");
-        this.setState({
-            selectedOptions: [...this.state.selectedOptions, this.optionsArray[1]]
-
-            // const selectedOptions = state.selectedOptions.concat(this.optionsArray[index]);
-            // const questionId = state.questionId.concat(id);
-        })
-        console.log(this.state.selectedOptions);
+    setSelectedOption(qid, response) {
+        // console.log(qid,this.optionsArray[response]);
+        if(this.selectedOptions.some(option=> option.qid === qid)){
+            for(let i=0;i<this.selectedOptions.length;i++){
+                if(this.selectedOptions[i].qid === qid){
+                    console.log("okay match");
+                    this.selectedOptions[i].response=this.optionsArray[response];
+                }
+            }
+        } else {
+            this.selectedOptions.push({qid:qid,response:this.optionsArray[response]})
+        }
+        console.log("inside setSelectedOption",this.selectedOptions);
     }
 
     gotoNextQuestion() {
-        if(this.state.currentQuestionIndex < this.state.quizQuestions.length - 1)
+        if(this.state.currentQuestionIndex < this.state.quizQuestions.length - 1) {
             this.setState({
                 currentQuestionIndex: this.state.currentQuestionIndex + 1
             })
+            // if(this.state.currentQuestionIndex === 9)
+            //     this.setState({
+            //         nextBtn: "Submit"
+            //     })
+        }
         else
             return
     }
@@ -105,16 +140,25 @@ class MgmtQuiz extends React.Component {
             this.getTimer();
     }
 
+    componentWillUnmount() {
+        this.submitQuiz();
+    }
+
     render() {
         if(!sessionStorage.getItem("Token")) {
             return(
                 <Redirect to="/" />
             )
         }
+        else if(this.state.time === 0) {
+            return(
+                <Redirect to="/thank-you" />
+            )
+        }
         return(
             <Background>
                 { this.state.quizQuestions.length === 0 ?
-                    <div>Loading...</div> :
+                    <div className="loading">Loading...</div> :
                     <>
                         <div className="heading">Management Quiz</div>
                         <div className="question-section">
@@ -124,28 +168,37 @@ class MgmtQuiz extends React.Component {
                             <div className="question-text">
                                 {this.state.quizQuestions[this.state.currentQuestionIndex].description}
                             </div>
-                            {/* <div className='answer-section'>
-                                {Object.keys(this.state.quizQuestions[this.state.currentQuestionIndex].options).map((key, index) => {
+                            <div className='answer-section'>
+                                <textarea className="mgmt-answer" onChange={ this.setSelectedOption() }></textarea>
+                                {/* {Object.keys(this.state.quizQuestions[this.state.currentQuestionIndex].options).map((key, index) => {
                                     return (
-                                        <div key={index}>
+                                        <div key={index} onClick={()=>this.setSelectedOption(this.state.quizQuestions[this.state.currentQuestionIndex]._id,index)}>
                                             <button className="options"
-                                                    onCLick={ () => this.setSelectedOption() }
                                                     value={this.optionsArray[index]}>
                                                     {this.optionsArray[index]}. {this.state.quizQuestions[this.state.currentQuestionIndex].options[key]}
                                             </button>
                                         </div>
                                     )
-                                })}
-                            </div> */}
-                            <div className='btn-bottom'>
-                            <button onClick={ () => { this.gotoPreviousQuestion() } }>Previous</button>
-                                <div>
-                                <Timer time={this.state.time} />
-                                <button className="submit-btn" onClick={()=>{this.showModal1()}}>Submit</button>
-                                </div>
-                                <button onClick={ () => { this.gotoNextQuestion() } }>Next</button>
-                                <Modal show={this.state.showModal} onHide={this.hideModal} />
+                                })} */}
                             </div>
+                            <div className='btn-bottom'>
+                                {this.state.currentQuestionIndex === 0 ?
+                                    <button disabled={ true } id="disabled-btn">Previous</button> :
+                                    <button onClick={ () => { this.gotoPreviousQuestion() } }>Previous</button>
+                                }
+                                {/* <button className={ submitButton } onCLick={ () => { this.showModal1() } }>Submit</button> */}
+                                {this.state.currentQuestionIndex === 4 ? 
+                                    <button onClick={ () => { this.showModal1() } }>Submit</button> :
+                                    <button onClick={ () => { this.gotoNextQuestion() } }>Next</button>
+                                }
+                                <Modal show={this.state.showModal} onHide={this.hideModal} submitQuiz={ this.submitQuiz } />
+                            </div>
+                            <div className="timer">
+                                <Timer time={this.state.time} />
+                            </div>
+                            {/* <div>
+                                <button onClick={ () => { this.submitQuiz } }>Submit</button>
+                            </div> */}
                         </div>
                     </>
                 }
