a8c30aa4b5a1d0a8090c71f2202dbb55e6e06998
ishan bharadwaj <ishan19731@gmail.com>
2020-12-27 00:54:56 +0530

    minor changes

--- src/components/Forms/SignUp/SignUp.js
+++ src/components/Forms/SignUp/SignUp.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import Recaptcha from "react-google-invisible-recaptcha";
 import adggif from "../../../assets/img/adggif.gif";
 import { Link } from "react-router-dom";
-import VerificationPage from "./verification";
 
 export class SignUp extends Component {
   state = {
